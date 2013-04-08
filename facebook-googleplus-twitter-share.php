154c561aa176cdfc0a154fd4faf0eae09ecfc06c
rranveer4 <rranveer4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-08 05:11:18 +0000

    Just update
    git-svn-id: https://plugins.svn.wordpress.org/facebook-google-twitter-share/trunk@693448 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ facebook-googleplus-twitter-share.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name: Facebook, Google+ & Twitter Share
+Plugin URI: 
+Description: Facebook, Google+ and Twitter share. Easy to use, just install and activate. No database table will be create. No javascript or js file required.
+Version: 1.00
+Author: Ranveer
+Author URI: http://www.ranveer.info
+*/
+function fgt_share_style() {
+	$myStyleUrl  = plugin_dir_url (__FILE__).'style.css';
+	$myStyleFile = plugin_dir_path(__FILE__).'style.css';
+	if ( file_exists($myStyleFile) ) {
+	    wp_register_style('really_simple_share_style', $myStyleUrl);
+	    wp_enqueue_style ('really_simple_share_style');
+	}
+}
+function fgt_share_add_hook($content) {
+	$permalink = get_permalink($post->ID);
+	$title 	   = get_the_title($ID);
+	$imagepath = plugins_url().'/facebook-googleplus-twitter-share/images/';
+	$content .= '<div class="sharelinks" style=""><span class="text">share on</span> 
+	<a title="'.$title.'" rel="nofollow" target="_blank" href="http://www.facebook.com/sharer.php?u='.$permalink.'"><img width="56" height="20" alt="facebook" src="'.$imagepath.'facebook.png"></a>
+	<a title="'.$title.'" rel="nofollow" target="_blank" href="https://plus.google.com/share?url='.$permalink.'"><img width="60" height="20" alt="google plus" src="'.$imagepath.'googleplus.png"></a>
+	<a title="'.$title.'" Nagar" rel="nofollow" target="_blank" href="http://twitter.com/home?status='.$title.' '.$permalink.'"><img width="70" height="20" alt="twitter" src="'.$imagepath.'twitter.png"></a>
+	</div>';
+	//$content .= plugin_basename();
+    return $content;
+}
+
+add_action('wp_print_styles', 'fgt_share_style');
+add_filter('the_content', 'fgt_share_add_hook');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== Facebook, Google+ & Twitter Share ===
+Contributors: rranveer4
+Donate link: 
+Tags: Facebook, Google Plus, Twitter Share
+Requires at least: 3.3
+Tested up to: 3.5.1
+Stable tag: Facebook, Google Plus, Twitter Share
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Facebook, Google+ and Twitter share. Easy to use, just install and activate. No database table will be create. No javascript or js file required.
+
+== Description ==
+
+Facebook, Google+ and Twitter share. Easy to use, just install and activate. No database table will be create. No javascript or js file required.
+
+== Installation ==
+
+1. Upload `facebook-googleplus-twitter-share` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently asked questions ==
+
+= A question that someone might have =
+
+An answer to that question.
+
+== Screenshots ==
+
+1. [Screenshot 1](http://smsbyheart.com)
+2. [Screenshot 2](http://ranveer.info)
+
+== Changelog ==
+
+
+
+== Upgrade notice ==
+
+
+
+== Arbitrary section 1 ==
+
